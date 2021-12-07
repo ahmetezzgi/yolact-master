a054be6740a7ba5f969423762bf80b49bdbebc7f
ahmetezzgi <47900619+ahmetezzgi@users.noreply.github.com>
2021-12-07 14:38:39 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -54,7 +54,7 @@ COCO_LABEL_MAP = { 1:  1,  2:  2,  3:  3,  4:  4,  5:  5,  6:  6,  7:  7,  8:  8
                   74: 65, 75: 66, 76: 67, 77: 68, 78: 69, 79: 70, 80: 71, 81: 72,
                   82: 73, 84: 74, 85: 75, 86: 76, 87: 77, 88: 78, 89: 79, 90: 80}
 
-DGLASS_CLASSES = ('ekstrem_sivi', 'catlak')
+DGLASS_CLASSES = ('catlak', 'ekstrem_sivi')
 
 DGLASS_LABEL_MAP = { 1:  1,  2:  2}
 
