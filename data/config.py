273d8602e4fa7071b03f7a7b18aaad251489c0e0
ahmetezzgi <ahmet.ezgi@bil.omu.edu.tr>
2021-12-09 16:26:42 +0300

    Update config.py

--- data/config.py
+++ data/config.py
@@ -679,7 +679,7 @@ yolact_base_config = coco_base_config.copy({
     'num_classes': len(dglass_dataset.class_names) + 1,
 
     # Image Size
-    'max_size': 512,
+    'max_size': 550,
     
     # Training params
     'lr_steps': (7000, 15000, 17500, 18750),
